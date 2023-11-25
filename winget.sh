386337690bc0d3374c68581ae11c8810e9fff3f4
ZeroMarker <u78k4l67@163.com>
2023-11-25 23:02:18 +0800

    winget

---
+++ winget.sh
@@ -0,0 +1,14 @@
+# life
+winget install Google.Chrome Google.GoogleDrive Thunder.Thunder MEGAsync Baidu.BaiduNetdisk iQIYI.GeePlayer SumatraPDF SiYuan -y
+# db
+winget install MySQL MongoDB.Compass.Full MongoDB PostgreSQL.PostgreSQL Microsoft.SQLServer.2022.Express -y
+# ide
+winget install JetBrains.Toolbox -y
+# sns
+winget install QQ WeChat Tencent.WeCom Tencent.TencentMeeting -y
+# ime
+winget install Google.JapaneseIME Rime.Weasel -y
+# tool
+winget install Bandizip GoldenDict JAMSoftware.TreeSize.Free ToDesk GeekUninstaller.GeekUninstaller Fndroid.ClashForWindows Notepad++ DiskGenius Listary Microsoft.PowerToys NickeManarin.ScreenToGif -y
+# dev
+winget install Git GitHub.GitHubDesktop DBeaver Ubuntu Microsoft.DevHome Microsoft.WindowsTerminal Node.js VMware.WorkstationPlayer Oracle.VirtualBox NxShell Microsoft.VisualStudioCode Docker.DockerDesktop GitHub.cli Apifox Postman -y
