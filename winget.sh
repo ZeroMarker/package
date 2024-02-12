959e550a7aab146f05f88edc10a43b82039b2166
Mark Chen <u78k4l67@163.com>
2024-02-12 19:52:39 +0800

    init

--- winget.sh
+++ winget.sh
@@ -1,5 +1,6 @@
 # life
 winget install Google.Chrome Google.GoogleDrive Thunder.Thunder MEGAsync Baidu.BaiduNetdisk iQIYI.GeePlayer SumatraPDF -y
+winget install Listen1 -y
 # db
 winget install MySQL MongoDB.Compass.Full MongoDB PostgreSQL.PostgreSQL Microsoft.SQLServer.2022.Express -y
 # ide
@@ -10,13 +11,17 @@ winget install QQ WeChat Tencent.WeCom Tencent.TencentMeeting -y
 winget install Google.JapaneseIME Rime.Weasel -y
 # tool
 winget install Bandizip GoldenDict JAMSoftware.TreeSize.Free ToDesk GeekUninstaller.GeekUninstaller carnac -y
-winget install Fndroid.ClashForWindows Notepad++ DiskGenius Listary Microsoft.PowerToys NickeManarin.ScreenToGif -y
+winget install 'Clash Verge' Notepad++ DiskGenius Listary Microsoft.PowerToys NickeManarin.ScreenToGif -y
 winget install ffmpeg -y
 # dev
-winget install Git GitHub.GitHubDesktop DBeaver Ubuntu Microsoft.DevHome Microsoft.WindowsTerminal Node.js -y
-winget install VMware.WorkstationPlayer Oracle.VirtualBox NxShell Microsoft.VisualStudioCode -y
-winget install Docker.DockerDesktop GitHub.cli Apifox Postman -y
+winget install Git 'GitHub Desktop' DBeaver Ubuntu Microsoft.DevHome Microsoft.WindowsTerminal Node.js -y
+winget install VMware.WorkstationPlayer Oracle.VirtualBox NxShell vscode -y
+winget install 'Docker Desktop' GitHub.cli Apifox Postman -y
 # sys
-winget install 'Sysinternals Suite'
-winget install Autoruns
-winget install 'Process Explorer'
+winget install 'Sysinternals Suite' -y
+winget install Autoruns -y
+winget install 'Process Explorer' -y
+
+# init
+winget install Google.Chrome DBeaver vscode git 'GitHub Desktop' 'Clash Verge' JetBrains.Toolbox Tencent.QQ.NT WeChat NxShell Bandizip Notepad++ Everything ToDesk 'Geek Uninstaller' 'Windows Terminal' -y
+winget install Microsoft.PowerToys -y
