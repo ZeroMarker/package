7193f24a215694f0ec537fd845e94d6237bee738
Mark Chen <u78k4l67@163.com>
2024-08-19 00:13:57 +0800

    npm

--- winget.sh
+++ winget.sh
@@ -1,27 +1,27 @@
 # life
-winget install Google.Chrome Google.GoogleDrive Thunder.Thunder MEGAsync Baidu.BaiduNetdisk iQIYI.GeePlayer SumatraPDF -y
-winget install Listen1 -y
+winget install Google.Chrome Google.GoogleDrive Thunder.Thunder MEGAsync Baidu.BaiduNetdisk iQIYI.GeePlayer SumatraPDF 
+winget install Listen1 
 # db
-winget install MySQL MongoDB.Compass.Full MongoDB PostgreSQL.PostgreSQL Microsoft.SQLServer.2022.Express -y
+winget install MySQL MongoDB.Compass.Full MongoDB PostgreSQL.PostgreSQL Microsoft.SQLServer.2022.Express 
 # ide
-winget install JetBrains.Toolbox -y
+winget install JetBrains.Toolbox 
 # sns
-winget install QQ WeChat Tencent.WeCom Tencent.TencentMeeting -y
+winget install QQ WeChat Tencent.WeCom Tencent.TencentMeeting 
 # ime
-winget install Google.JapaneseIME Rime.Weasel -y
+winget install Google.JapaneseIME Rime.Weasel 
 # tool
-winget install Bandizip GoldenDict JAMSoftware.TreeSize.Free ToDesk GeekUninstaller.GeekUninstaller carnac -y
-winget install 'Clash Verge' Notepad++ DiskGenius Listary Microsoft.PowerToys NickeManarin.ScreenToGif -y
-winget install ffmpeg Telerik.Fiddler.Classic -y
+winget install Bandizip GoldenDict JAMSoftware.TreeSize.Free ToDesk GeekUninstaller.GeekUninstaller carnac 
+winget install 'Clash Verge' Notepad++ DiskGenius Listary Microsoft.PowerToys NickeManarin.ScreenToGif 
+winget install ffmpeg Telerik.Fiddler.Classic 
 # dev
-winget install Git 'GitHub Desktop' DBeaver Ubuntu Microsoft.DevHome Microsoft.WindowsTerminal Node.js -y
-winget install VMware.WorkstationPlayer Oracle.VirtualBox NxShell vscode -y
-winget install 'Docker Desktop' GitHub.cli Apifox Postman -y
+winget install Git 'GitHub Desktop' DBeaver Ubuntu Microsoft.DevHome Microsoft.WindowsTerminal Node.js 
+winget install VMware.WorkstationPlayer Oracle.VirtualBox NxShell vscode 
+winget install 'Docker Desktop' GitHub.cli Apifox Postman 
 # sys
-winget install 'Sysinternals Suite' -y
-winget install Autoruns -y
-winget install 'Process Explorer' -y
+winget install 'Sysinternals Suite' 
+winget install Autoruns 
+winget install 'Process Explorer' 
 
 # init
-winget install Google.Chrome DBeaver vscode git 'GitHub Desktop' 'Clash Verge' JetBrains.Toolbox Tencent.QQ.NT WeChat NxShell Bandizip Notepad++ Everything ToDesk 'Geek Uninstaller' 'Windows Terminal' -y
-winget install Microsoft.PowerToys -y
+winget install Google.Chrome DBeaver vscode git 'GitHub Desktop' 'Clash Verge' JetBrains.Toolbox Tencent.QQ.NT WeChat NxShell Bandizip Notepad++ Everything ToDesk 'Geek Uninstaller' 'Windows Terminal' 
+winget install Microsoft.PowerToys 
